2f05b1561838e75f016065cdc690c7d80dcd8a0a
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-12-08 15:39:26 +1300

    Update creativecloud.sh

--- creativecloud.sh
+++ creativecloud.sh
@@ -13,8 +13,8 @@ PREFIX="$HOME/CreativeCloud"
 WINETRICKS="msxml3 atmlib corefonts fontsmooth=rgb gdiplus"
 WINETRICKS_DOWNLOAD="https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
 ADOBE_DOWNLOAD="http://ccmdl.adobe.com/AdobeProducts/KCCC/CCD/4_9/win32/ACCCx4_9_0_504.zip"
-GECKO_32_DOWNLOAD="http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86.msi"
-GECKO_64_DOWNLOAD="http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86_64.msi"
+GECKO_32_DOWNLOAD="https://dl.winehq.org/wine/wine-gecko/2.47.3/wine-gecko-2.47.3-x86.msi"
+GECKO_64_DOWNLOAD="https://dl.winehq.org/wine/wine-gecko/2.47.3/wine-gecko-2.47.3-x86_64.msi"
 
 #TODO: Check Wine + winbind is installed
 
